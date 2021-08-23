b8de6dfd85041aaaf82eef24be3211edc6d9102c
WhenixD <whenix.developer@gmail.com>
2021-08-23 18:58:56 -0500

    Add JavaScript components

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+// import './App.css';
+
+const todos = [
+  {
+    text: 'Design landing page',
+    completed: false,
+  },
+  {
+    text: 'Program landing page',
+    completed: false,
+  },
+  {
+    text: 'Deploy landing page',
+    completed: false,
+  },
+];
+
+function App(props) {
+  return (
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export default App;
