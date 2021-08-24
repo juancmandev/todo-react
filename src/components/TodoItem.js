4d96512aefcdd9be5cef47419dafb5e65fa9239b
WhenixD <whenix.developer@gmail.com>
2021-08-24 18:13:04 -0500

    Add Complete and Delete Todo's

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,26 +2,18 @@ import React from 'react';
 import '../styles/TodoItem.css';
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert('You have completed the ToDo "' + props.text + '"');
-  };
-
-  const onDelete = () => {
-    alert('You have delete the ToDo "' + props.text + '"');
-  };
-
   return (
     <li className='TodoItem'>
       <span
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className='Icon Icon-delete' onClick={onDelete}>
+      <span className='Icon Icon-delete' onClick={props.onDelete}>
         X
       </span>
     </li>
