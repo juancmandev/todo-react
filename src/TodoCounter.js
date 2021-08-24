9a32fc5726f5e2f707287d00f52197c4b8425b5b
WhenixD <whenix.developer@gmail.com>
2021-08-23 19:16:32 -0500

    Add TodoCounter css

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './TodoCounter.css';
 
 function TodoCounter() {
-  return <h2>You've completed 2 of 3 TODOs</h2>;
+  return <h2 className='TodoCounter'>You've completed 2 of 3 TODOs</h2>;
 }
 
 export { TodoCounter };
